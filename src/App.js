c9ca3f7c5137ed240f910ae56402e3ba57b1d2d9
Esteban Carter <carterea1999@gmail.com>
2021-06-11 14:58:44 -0400

    Working blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Component} from 'react';
 import './App.css';
+import BlogForm from './BlogForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      blogs: [],
+      counter: 2,
+    }
+    this.addBlog = this.addBlog.bind(this);
+  }
+  componentDidMount(){
+    const blogs = [
+    {
+      id: 1,
+      title: 'A blog post Title!',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    }
+  ];
+  this.setState({ blogs });
+  }
+  addBlog( blog ){
+    blog.id = this.state.counter;
+    const blogs = [ ...this.state.blogs ];
+    blogs.push(blog);
+    this.setState((state) => ({ blogs, counter: state.counter + 1 }));
+  }
+  render(){
+    return (
+      <div>
+        <h1>Blog Form</h1>
+        <BlogForm addBlog={this.addBlog} />
+      </div>
+    );
+  }
 }
 
 export default App;
